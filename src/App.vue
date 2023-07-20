bb2fac835a082d63ba1ddf2ced977568346d524d
suozhao <296954131@qq.com>
2023-07-21 00:50:40 +0800

    公转&轨道&背景

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <script setup>
 import * as THREE from 'three';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
+import { onMounted } from 'vue';
 
 
 const scene = new THREE.Scene();
+const starrySkyTexture = new THREE.TextureLoader().load('stars.jpeg');
+scene.background = starrySkyTexture;
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 200;
+camera.position.y = 200;
+camera.position.z = 500;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+// 添加控制器
+const controls = new OrbitControls(camera, renderer.domElement);
 
 const sunGeometry = new THREE.SphereGeometry(10, 32, 32);
 const sunTexture = new THREE.TextureLoader().load('1.jpg');
@@ -29,7 +35,7 @@ const planetsData = [
     name: 'Venus',
     radius: 7,
     texture: '3.jpg',
-    distance: 40,
+    distance: 60,
     rotationSpeed: 0.02,
     revolutionSpeed: 0.015
   },
@@ -37,7 +43,7 @@ const planetsData = [
     name: 'Earth',
     radius: 7.5,
     texture: '4.jpg',
-    distance: 50,
+    distance: 90,
     rotationSpeed: 0.03,
     revolutionSpeed: 0.01
   },
@@ -45,7 +51,7 @@ const planetsData = [
     name: 'Mars',
     radius: 6,
     texture: '5.jpg',
-    distance: 60,
+    distance: 120,
     rotationSpeed: 0.04,
     revolutionSpeed: 0.008
   },
@@ -53,7 +59,7 @@ const planetsData = [
     name: 'Jupiter',
     radius: 15,
     texture: '6.jpg',
-    distance: 70,
+    distance: 150,
     rotationSpeed: 0.02,
     revolutionSpeed: 0.005
   },
@@ -61,7 +67,7 @@ const planetsData = [
     name: 'Saturn',
     radius: 12,
     texture: '7.jpg',
-    distance: 80,
+    distance: 180,
     rotationSpeed: 0.01,
     revolutionSpeed: 0.004
   },
@@ -69,7 +75,7 @@ const planetsData = [
     name: 'Uranus',
     radius: 10,
     texture: '8.jpg',
-    distance: 90,
+    distance: 210,
     rotationSpeed: 0.015,
     revolutionSpeed: 0.003
   },
@@ -77,7 +83,7 @@ const planetsData = [
     name: 'Neptune',
     radius: 9,
     texture: '9.jpg',
-    distance: 100,
+    distance: 240,
     rotationSpeed: 0.012,
     revolutionSpeed: 0.002
   }
@@ -87,6 +93,27 @@ const planetMaterials = planetsData.map(planet => {
   const texture = new THREE.TextureLoader().load(planet.texture);
   return new THREE.MeshPhongMaterial({ map: texture });
 });
+// 创建行星公转轨道
+function createOrbit(radius) {
+  const segments = 360;
+  const material = new THREE.LineBasicMaterial({ color: 0xffffff ,linewidth: 0.1, transparent:true,opacity:0.2});
+  const points = [];
+  for (let i = 0; i <= segments; i++) {
+    const theta = (i / segments) * Math.PI * 2;
+    const x = radius * Math.cos(theta);
+    const z = radius * Math.sin(theta);
+    points.push(new THREE.Vector3(x, 0, z));
+  }
+  const geometry = new THREE.BufferGeometry().setFromPoints(points);
+  const orbit = new THREE.Line(geometry, material);
+  scene.add(orbit);
+}
+
+    // // 创建行星公转轨道
+    // createOrbit(0.39 * 50); // 水星
+    // createOrbit(0.72 * 50); // 金星
+    // createOrbit(1 * 50);    // 地球
+    // createOrbit(1.52 * 50); // 火星
 
 const planets = planetMaterials.map((material, index) => {
   const planetGeometry = new THREE.SphereGeometry(planetsData[index].radius, 32, 32);
@@ -95,6 +122,7 @@ const planets = planetMaterials.map((material, index) => {
   planet.rotationSpeed = planetsData[index].rotationSpeed;
   planet.revolutionSpeed = planetsData[index].revolutionSpeed;
   scene.add(planet);
+  createOrbit(planetsData[index].distance)
   return planet;
 });
 
@@ -114,20 +142,34 @@ function animate() {
     const distance = planet.distance;
 
     // 行星公转
-    planet.position.x = Math.cos(revolutionSpeed) * distance;
-    planet.position.y = Math.sin(revolutionSpeed) * distance;
+    const angle = Date.now() * revolutionSpeed / 10;
+    let x = Math.cos(angle) * distance;
+    let z = Math.sin(angle) * distance;
+    planet.position.set(x,0,z)
 
     // 行星自转
     planet.rotation.y += rotationSpeed;
   });
+  sun.rotation.y += 0.01;
 
   renderer.render(scene, camera);
 }
 
-animate();
+onMounted(() => {
+  document.getElementById('milky').appendChild(renderer.domElement);
+  animate();
+});
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
 </script>
 
 <template>
+  <div id="app">
+    <div id="milky"></div>
+  </div>
 </template>
 
 <style scoped>
